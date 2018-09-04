4bc897abcd2908442b6b2417d0e6dd3d2482ba7d
ElephantFrontend <32238103+ElephantFrontend@users.noreply.github.com>
2018-09-04 20:32:54 +0300

    new commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 4000
+    })
+    $('.gamburger_header').on('click', function(){
+        $('.menu_header').toggleClass('menu_header-active');
+
+    })
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImpRdWVyeShkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24oKXtcclxuICAgICQoJy5jYXJvdXNlbCcpLmNhcm91c2VsKHtcclxuICAgICAgICBpbnRlcnZhbDogNDAwMFxyXG4gICAgfSlcclxuICAgICQoJy5nYW1idXJnZXJfaGVhZGVyJykub24oJ2NsaWNrJywgZnVuY3Rpb24oKXtcclxuICAgICAgICAkKCcubWVudV9oZWFkZXInKS50b2dnbGVDbGFzcygnbWVudV9oZWFkZXItYWN0aXZlJyk7XHJcblxyXG4gICAgfSlcclxufSk7Il0sImZpbGUiOiJtYWluLmpzIn0=
